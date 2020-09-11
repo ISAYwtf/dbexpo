c114107627d22fca7685b1fb19564e70bfeaf6f9
Iskander Aydynov <iskander.aydynov@mail.ru>
2020-09-11 19:47:07 +0300

    Добавлены 4 секции и header в index.html

---
+++ assets/js/slider.js
@@ -0,0 +1,34 @@
+let sliders = document.querySelectorAll('.swiper-container');
+
+sliders.forEach(el => {
+    let mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        setWrapperSize: true,
+        slidesPerView: '1',
+        centeredSlides: true,
+
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: true
+        },
+
+        // Navigation arrows
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+    });
+
+    let fadeTxt = index => {
+        let slideTxt = el.querySelectorAll(".swiper__txt--item");
+
+        slideTxt.forEach(item => {
+            item.classList.remove("swiper__txt--active");
+        });
+        slideTxt[index].classList.add("swiper__txt--active");
+    };
+
+    mySwiper.on('slideChange', function() {
+        fadeTxt(mySwiper.activeIndex);
+    });
+});
