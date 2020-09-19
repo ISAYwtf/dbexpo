b59bbf64a18cf771fa21cf375598f0b06ca89e78
Iskander Aydynov <iskander.aydynov@mail.ru>
2020-09-19 21:37:06 +0300

    Остался футер в index.html

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,34 +1,123 @@
-let sliders = document.querySelectorAll('.slider');
+// let sliders = document.querySelectorAll('.slider');
 
-sliders.forEach(el => {
-    let mySwiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        setWrapperSize: true,
-        slidesPerView: '1',
-        centeredSlides: true,
+// sliders.forEach(el => {
+//     let mySwiper = new Swiper('.swiper-container', {
+//         // Optional parameters
+//         setWrapperSize: true,
+//         slidesPerView: '1',
+//         centeredSlides: true,
 
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: true
-        },
+//         autoplay: {
+//             delay: 5000,
+//             disableOnInteraction: true
+//         },
 
-        // Navigation arrows
-        navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-        },
+//         // Navigation arrows
+//         navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//         },
+//     });
+
+//     let fadeTxt = index => {
+//         let slideTxt = el.querySelectorAll(".swiper__txt--item");
+
+//         slideTxt.forEach(item => {
+//             item.classList.remove("swiper__txt--active");
+//         });
+//         slideTxt[index].classList.add("swiper__txt--active");
+//     };
+
+//     mySwiper.on('slideChange', () => {
+//         fadeTxt(mySwiper.activeIndex);
+//     });
+// });
+
+let fadeTxt = (index, slider) => {
+    let slideTxt = slider.querySelectorAll(".swiper__txt--item");
+
+    slideTxt.forEach(item => {
+        item.classList.remove("swiper__txt--active");
     });
+    slideTxt[index].classList.add("swiper__txt--active");
+};
 
-    let fadeTxt = index => {
-        let slideTxt = el.querySelectorAll(".swiper__txt--item");
+let sliderOffer = document.querySelector('.slider-offer'),
+    bigSlider = document.querySelector('.big-slider'),
+    newSlider = document.querySelector('.news-slider');
 
-        slideTxt.forEach(item => {
-            item.classList.remove("swiper__txt--active");
-        });
-        slideTxt[index].classList.add("swiper__txt--active");
-    };
+let swiperOffer = new Swiper('.swiper-offer', {
+    // Optional parameters
+    setWrapperSize: true,
+    slidesPerView: '1',
+    centeredSlides: true,
 
-    mySwiper.on('slideChange', function() {
-        fadeTxt(mySwiper.activeIndex);
-    });
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-offer-next',
+        prevEl: '.swiper-offer-prev',
+    },
+});
+
+swiperOffer.on('slideChange', () => fadeTxt(swiperOffer.activeIndex, sliderOffer));
+
+let bigSwiper = new Swiper('.big-swiper', {
+    // Optional parameters
+    setWrapperSize: true,
+    slidesPerView: '1',
+    centeredSlides: true,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.big-swiper-next',
+        prevEl: '.big-swiper-prev',
+    },
+});
+
+let centerBigSwiper = new Swiper('.center-swiper', {
+    // Optional parameters
+    setWrapperSize: true,
+    slidesPerView: '1',
+    centeredSlides: true,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.big-swiper-next',
+        prevEl: '.big-swiper-prev',
+    },
+});
+
+centerBigSwiper.on('slideChange', () => fadeTxt(centerBigSwiper.activeIndex, bigSlider));
+
+let newSwiper = new Swiper('.news-swiper', {
+    // Optional parameters
+    setWrapperSize: true,
+    slidesPerView: '1',
+    centeredSlides: true,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.news-swiper-next',
+        prevEl: '.news-swiper-prev',
+    },
 });
