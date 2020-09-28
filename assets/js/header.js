704d44475a8b7e46cafbaade66746e2397b466d5
Iskander Aydynov <iskander.aydynov@mail.ru>
2020-09-29 00:56:59 +0300

    Адаптивность под планшет для шапки

---
+++ assets/js/header.js
@@ -0,0 +1,40 @@
+let header = document.querySelector('header'),
+    logo = header.querySelector('.header__logo'),
+    logoWidthPercent = (logo.clientWidth * 100) / header.querySelector('.wrap').clientWidth,
+    logoWidth = logo.clientWidth;
+
+if (window.screen.width > 768 && window.screen.width <= 1024) {
+    if (window.scrollY > 200) {
+        logo.style.width = 47 + 'px';
+    }
+} else if (window.screen.width > 560 && window.screen.width <= 1024) {
+    if (window.scrollY > 130) {
+        logo.style.width = 47 + 'px';
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (window.screen.width > 560 && window.screen.width <= 1024) {
+        logo.style.width = logoWidthPercent + '%';
+    } else if (window.screen.width <= 560) {
+        logo.style.width = 10 + '%';
+    }
+    logoWidth = logo.clientWidth;
+}, false);
+
+window.addEventListener('scroll', () => {
+
+    if (window.screen.width > 768 && window.screen.width <= 1024) {
+        if (window.scrollY <= 200) {
+            logo.style.width = logoWidth - (window.scrollY / 2) + 'px';
+        } else if (window.scrollY > 200) {
+            logo.style.width = 47 + 'px';
+        }
+    } else if (window.screen.width > 560 && window.screen.width <= 1024) {
+        if (window.scrollY <= 130) {
+            logo.style.width = logoWidth - (window.scrollY / 2) + 'px';
+        } else if (window.scrollY > 130) {
+            logo.style.width = 47 + 'px';
+        }
+    }
+});
